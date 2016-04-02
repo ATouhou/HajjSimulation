89d0358522695391e68d84e312127d89f4fcd725
alexstewart1997 <stewarad@usc.edu>
2016-04-02 13:55:05 -0700

    Added files via upload

---
+++ Spawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+public class Spawner : MonoBehaviour
+{
+    public float spawn = 2;
+    public GameObject bullet;
+
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawn -= Time.deltaTime;
+        if (spawn < 0)
+        {
+            Vector3 myPosition = GetComponent<Transform>().position;
+            myPosition.y += 0;
+
+            Quaternion myRotation = GetComponent<Transform>().rotation;
+
+            Instantiate(bullet, myPosition, myRotation);
+
+            spawn = 5;
+           
+        }
+    }
+
+}
