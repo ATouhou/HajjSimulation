57b0cd15de765927d1af368d5479b9a1003b7591
VivekR10 <Vivek.S.Ramachandran@gmail.com>
2016-04-03 18:19:50 -0700

    Added files via upload

--- MoveTo.cs
+++ MoveTo.cs
@@ -5,16 +5,20 @@
     public class MoveTo : MonoBehaviour {
        
        public Transform goal;
-       
-       
+       public int randomMax;
+       public int agentRadius;
+       public int tripFactor;
        void Start () {
           NavMeshAgent agent = GetComponent<NavMeshAgent>();
           agent.destination = goal.position; 
+
        }
 
        void Update()
        {
-        int rand = Random.Range(0,500);
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, agentRadius);
+        int numColliders = hitColliders.Length;
+        int rand = Random.Range(0,randomMax)*numColliders;
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         if(agent.speed == 0)
         {
@@ -23,7 +27,7 @@
         else
         {
          
-          if(rand == 0)
+          if(rand >= tripFactor)
           {
             agent.speed = 0;
           }
