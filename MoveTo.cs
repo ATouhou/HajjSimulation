aca9dd0ee8954ff74e46de8569215dc9baa2b107
VivekR10 <Vivek.S.Ramachandran@gmail.com>
2016-04-03 17:19:48 -0700

    Added files via upload

--- MoveTo.cs
+++ MoveTo.cs
@@ -1,13 +1,36 @@
-﻿  // MoveTo.cs
+  // MoveTo.cs
     using UnityEngine;
     using System.Collections;
-    
+   
     public class MoveTo : MonoBehaviour {
        
        public Transform goal;
+       
        
        void Start () {
           NavMeshAgent agent = GetComponent<NavMeshAgent>();
           agent.destination = goal.position; 
        }
+
+       void Update()
+       {
+        int rand = Random.Range(0,500);
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        if(agent.speed == 0)
+        {
+
+        }
+        else
+        {
+         
+          if(rand == 0)
+          {
+            agent.speed = 0;
+          }
+          else {
+
+          }
+        }
+       }
     }
+
