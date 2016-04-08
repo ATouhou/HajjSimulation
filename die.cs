f71e0c4531c4bbaacd05102a5786403b5538bab2
alexstewart1997 <stewarad@usc.edu>
2016-04-07 18:02:37 -0700

    Added files via upload

---
+++ die.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class die : MonoBehaviour {
+    public float destroytime = 2;
+	// Use this for initialization
+	void Start () {
+        Destroy(this.gameObject, destroytime);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+
+}
