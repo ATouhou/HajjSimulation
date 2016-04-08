de6b96d6015f1154aec3a39b9d15784dc7948b51
alexstewart1997 <stewarad@usc.edu>
2016-04-07 18:21:09 -0700

    Added files via upload

---
+++ End.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class End : MonoBehaviour {
+
+        public float position = 2;
+    // Use this for initialization
+    void Start()
+    {
+        if (transform.position.z >= position)
+        {
+            Destroy(gameObject);
+        }
+    }
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
