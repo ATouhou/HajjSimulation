88b239d1e1dd845fa55bde4711d029b7ab3196b1
VivekR10 <Vivek.S.Ramachandran@gmail.com>
2016-04-12 14:10:02 -0700

    4-12-16 Move Script
    New script with age factors

--- MoveTo.cs
+++ MoveTo.cs
@@ -1,25 +1,50 @@
   // MoveTo.cs
     using UnityEngine;
     using System.Collections;
+    
    
     public class MoveTo : MonoBehaviour {
        
        public Transform goal;
-       public int randomMax;
-       public int agentRadius;
-       public int tripFactor;
+       public float randomMax;
+       public float agentRadius;
+       public float tripFactor;
+       public float a,b;
+       public int minAge;
+       public int maxAge;
+       public float age, ageFactor;
+
+
+     // Equation: a()
+     // public GUIText scoreText;
        void Start () {
           NavMeshAgent agent = GetComponent<NavMeshAgent>();
           agent.destination = goal.position; 
+          age = Random.Range(minAge, maxAge);
+          
+          
+          
+          ageFactor = b*((float)0.00156*(Mathf.Pow(age-(float)20, (float)1.409)));
 
        }
 
        void Update()
        {
+         
+
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, agentRadius);
-        int numColliders = hitColliders.Length;
-        int rand = Random.Range(0,randomMax)*numColliders;
+        float numColliders = hitColliders.Length;
+        float rand = a*(Random.Range(0,randomMax)*numColliders)+ ageFactor;
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
+     //   scoreText.text = score.ToString();
+        if(age >= 65 && agent.speed != 0)
+          {
+            GetComponent<Renderer>().material.color = Color.blue;
+          } 
+          else
+          {
+
+          }
         if(agent.speed == 0)
         {
 
@@ -30,6 +55,8 @@
           if(rand >= tripFactor)
           {
             agent.speed = 0;
+            GetComponent<Renderer>().material.color = Color.red;
+       //     score++;
           }
           else {
 
@@ -37,4 +64,3 @@
         }
        }
     }
-
